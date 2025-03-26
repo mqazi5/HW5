8f5a0877992e005bd064afcb8e9f58a905edffc6
mqazi5 <141670970+mqazi5@users.noreply.github.com>
2025-03-26 17:24:58 -0500

    Update ProblemSolutions.java

--- ProblemSolutions.java
+++ ProblemSolutions.java
@@ -1,6 +1,6 @@
 /******************************************************************
  *
- *   YOUR NAME / SECTION NUMBER
+ *  Mohammad Qazi / SECTION 001
  *
  *   This java file contains the problem solutions of isSubSet, findKthLargest,
  *   and sort2Arrays methods. You should utilize the Java Collection Framework for
